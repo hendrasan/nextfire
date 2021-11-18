b64b7346163669bfdacc251e2e34b7b07e276ace
Hendra Susanto <anotherhendrasusanto@gmail.com>
2021-11-18 23:13:38 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import toast from "react-hot-toast";
+
+export default function Home() {
+  return (
+    <div>
+      <button onClick={() => toast.success("YES")}>Toast Me</button>
+    </div>
+  );
+}
